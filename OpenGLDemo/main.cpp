e47f712be1dfa72dc74bc10a0d33050e4296808f
ucchen <uc_chen@126.com>
2019-05-17 11:31:59 +0800

    使用EBO渲染一个正方形

--- OpenGLDemo/main.cpp
+++ OpenGLDemo/main.cpp
@@ -79,25 +79,25 @@ int main()
 	glDeleteShader(vertexShader);
 	glDeleteShader(fragmentShader);
 
-// 	GLfloat vertices[] = {
-// 		-0.5f, -0.5f, 0.0f,
-// 		0.5f, -0.5f, 0.0f,
-// 		0.0f,  0.5f, 0.0f
-// 	};
 	GLfloat vertices[] = {
-		// 第一个三角形
 		0.5f, 0.5f, 0.0f,   // 右上角
-		0.5f, -0.5f, 0.0f,  // 右下角
-		-0.5f, 0.5f, 0.0f,  // 左上角
-							// 第二个三角形
 		0.5f, -0.5f, 0.0f,  // 右下角
 		-0.5f, -0.5f, 0.0f, // 左下角
 		-0.5f, 0.5f, 0.0f   // 左上角
+	};
+	GLuint indices[] = {
+		0, 1, 3,
+		1, 2, 3
 	};
 
 	GLuint VAO;
 	glGenVertexArrays(1, &VAO);
 	glBindVertexArray(VAO);
+
+	GLuint EBO;
+	glGenBuffers(1, &EBO);
+	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
+	glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
 
 	GLuint VBO;
 	glGenBuffers(1, &VBO);
@@ -107,6 +107,7 @@ int main()
 	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
 	glEnableVertexAttribArray(0);
 
+	glBindBuffer(GL_ARRAY_BUFFER, 0);
 	glBindVertexArray(0);
 
 	while (!glfwWindowShouldClose(window))
@@ -118,12 +119,16 @@ int main()
 
 		glUseProgram(shaderProgram);
 		glBindVertexArray(VAO);
-		glDrawArrays(GL_TRIANGLES, 0, 6);
+		glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
 		glBindVertexArray(0);
 
 		glfwSwapBuffers(window);
 		glfwPollEvents();
 	}
+
+	glDeleteBuffers(1, &VAO);
+	glDeleteBuffers(1, &VBO);
+	glDeleteBuffers(1, &EBO);
 
 	glfwTerminate();
 	return 0;
