c74d5e9f4708942f28f2b150bb0f736a23e7e64a
ucchen <uc_chen@126.com>
2019-05-27 20:49:18 +0800

    创建10个旋转立方体

--- OpenGLDemo/main.cpp
+++ OpenGLDemo/main.cpp
@@ -89,6 +89,19 @@ int main()
 // 		1, 2, 3  // second triangle
 // 	};
 
+	glm::vec3 cubePositions[] = {
+		glm::vec3(0.0f,  0.0f,  0.0f),
+		glm::vec3(2.0f,  5.0f, -15.0f),
+		glm::vec3(-1.5f, -2.2f, -2.5f),
+		glm::vec3(-3.8f, -2.0f, -12.3f),
+		glm::vec3(2.4f, -0.4f, -3.5f),
+		glm::vec3(-1.7f,  3.0f, -7.5f),
+		glm::vec3(1.3f, -2.0f, -2.5f),
+		glm::vec3(1.5f,  2.0f, -2.5f),
+		glm::vec3(1.5f,  0.2f, -1.5f),
+		glm::vec3(-1.3f,  1.0f, -1.5f)
+	};
+
 	GLuint VAO;
 	glGenVertexArrays(1, &VAO);
 	glBindVertexArray(VAO);
@@ -182,21 +195,26 @@ int main()
 		ourShader.use();
 		ourShader.setFloat("mixValue", mixValue);
 
-		ourShader.use();
-		glm::mat4 model = glm::mat4(1);
-		model = glm::rotate(model, (float)glfwGetTime() * glm::radians(50.0f), glm::vec3(0.5f, 1.0f, 0.0f));
+		for (int i = 0; i < 10; ++i)
+		{
+			ourShader.use();
+			glm::mat4 model = glm::mat4(1);
+			model = glm::translate(model, cubePositions[i]);
+			model = glm::rotate(model, (float)glfwGetTime() * glm::radians(50.0f), glm::vec3(0.5f, 1.0f, 0.0f));
 
-		glm::mat4 view = glm::mat4(1);
-		view = glm::translate(view, glm::vec3(0.f, 0.f, -3.f));
+			glm::mat4 view = glm::mat4(1);
+			view = glm::translate(view, glm::vec3(0.f, 0.f, -3.f));
 
-		glm::mat4 projection = glm::mat4(1);
-		projection = glm::perspective(glm::radians(45.f), (float)screenWidth / srceenHeight, 0.1f, 100.f);
+			glm::mat4 projection = glm::mat4(1);
+			projection = glm::perspective(glm::radians(45.f), (float)screenWidth / srceenHeight, 0.1f, 100.f);
 
-		ourShader.setMat4("model", glm::value_ptr(model));
-		ourShader.setMat4("view", glm::value_ptr(view));
-		ourShader.setMat4("projection", glm::value_ptr(projection));
+			ourShader.setMat4("model", glm::value_ptr(model));
+			ourShader.setMat4("view", glm::value_ptr(view));
+			ourShader.setMat4("projection", glm::value_ptr(projection));
 
-		glDrawArrays(GL_TRIANGLES, 0, 36);
+			glDrawArrays(GL_TRIANGLES, 0, 36);
+		}
+
 
 		glfwSwapBuffers(window);
 		glfwPollEvents();
